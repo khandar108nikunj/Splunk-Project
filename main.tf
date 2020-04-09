ebd626988f081a31b098e7493f167a33dd380568
NIKUNJKUMAR KHANDAR <32493361+khandar108nikunj@users.noreply.github.com>
2020-04-09 19:53:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,65 @@ provider "aws" {
 	region = "us-east-1"
 }
 
-resource "aws_instance" "web_server" {
-	count = 1
-	ami = "ami-0915e09cc7ceee3ab"
-	instance_type = "t2.medium"
+data "template_file" "userdata_client" {
+  template = "${file("templates/userdata_client.tpl")}"
+}
+
+resource "aws_instance" "c0m1" {
+	ami = "ami-0affd4508a5d2481b"
+	instance_type = "t2.micro"
 	key_name = "Project"
+	user_data = "${data.template_file.userdata_client.rendered}"
 	security_groups = [ "project" ]
+	tags = {
+  	Name="Cluster_master"
+  }
+
+}
+
+resource "aws_instance" "i-1" {
+	ami = "ami-0affd4508a5d2481b"
+	instance_type = "t2.micro"
+	key_name = "Project"
+	user_data = "${data.template_file.userdata_client.rendered}"
+	security_groups = [ "project" ]
+	tags = {
+  	Name="Indexer-1"
+  }
+}
+
+resource "aws_instance" "i-2" {
+	ami = "ami-0affd4508a5d2481b"
+	instance_type = "t2.micro"
+	key_name = "Project"
+	user_data = "${data.template_file.userdata_client.rendered}"
+	security_groups = [ "project" ]
+	tags = {
+  	Name="Indexer-2"
+  }
+
+}
+
+resource "aws_instance" "sh" {
+	ami = "ami-0affd4508a5d2481b"
+	instance_type = "t2.micro"
+	key_name = "Project"
+	user_data = "${data.template_file.userdata_client.rendered}"
+	security_groups = [ "project" ]
+	tags = {
+  	Name="SH"
+  }
+
+}
+
+resource "aws_instance" "uf" {
+	ami = "ami-0affd4508a5d2481b"
+	instance_type = "t2.micro"
+	key_name = "Project"
+	user_data = "${data.template_file.userdata_client.rendered}"
+	security_groups = [ "project" ]
+	tags = {
+  	Name="UF"
+  }
+
 }
