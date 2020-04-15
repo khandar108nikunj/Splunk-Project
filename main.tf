b3b7571836d9515022bb7ddb4bfcd0f55acadda3
NIKUNJKUMAR KHANDAR <32493361+khandar108nikunj@users.noreply.github.com>
2020-04-15 20:39:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-bucket-nikunj"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
 	region = "us-east-1"
 }
