e6548628e55b723cb0aec941cc90740d034cc248
NIKUNJKUMAR KHANDAR <32493361+khandar108nikunj@users.noreply.github.com>
2020-04-20 14:26:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,14 +18,14 @@ resource "aws_instance" "c0m1" {
 	ami = "ami-0affd4508a5d2481b"
 	instance_type = "t2.micro"
 	key_name = "Project"
-	resource "aws_eip" "c0m1" {
-		instance = element(aws_instance.c0m1.*.id)
-	}
 	user_data = "${data.template_file.userdata_client.rendered}"
 	security_groups = [ "project" ]
 	tags = {
   	Name="C0m1"
   }
+	resource "aws_eip" "c0m1" {
+		instance = element(aws_instance.c0m1.*.id)
+	}
 }
 
 
@@ -33,29 +33,28 @@ resource "aws_instance" "idx-1" {
 	ami = "ami-0affd4508a5d2481b"
 	instance_type = "t2.micro"
 	key_name = "Project"
-	resource "aws_eip" "idx-1" {
-		instance = element(aws_instance.idx-1.*.id)
-	}
 	user_data = "${data.template_file.userdata_client.rendered}"
 	security_groups = [ "project" ]
 	tags = {
   	Name="Idx-1"
   }
+	resource "aws_eip" "idx-1" {
+		instance = element(aws_instance.idx-1.*.id)
+	}
 }
 
 resource "aws_instance" "idx-2" {
 	ami = "ami-0affd4508a5d2481b"
 	instance_type = "t2.micro"
 	key_name = "Project"
-	resource "aws_eip" "idx-2" {
-		instance = element(aws_instance.idx-2.*.id)
-	}
 	user_data = "${data.template_file.userdata_client.rendered}"
 	security_groups = [ "project" ]
 	tags = {
   	Name="Idx-2"
   }
-
+	resource "aws_eip" "idx-2" {
+		instance = element(aws_instance.idx-2.*.id)
+	}
 }
 
 resource "aws_instance" "sh" {
