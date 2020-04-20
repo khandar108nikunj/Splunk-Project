c1ede6ac8ca6e3ff1d34cd81abe487e66a5b51d2
NIKUNJKUMAR KHANDAR <32493361+khandar108nikunj@users.noreply.github.com>
2020-04-20 14:40:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,8 @@ data "template_file" "userdata_client" {
 resource "aws_eip" "c0m1" {
 	ami = "ami-0affd4508a5d2481b"
 	instance_type = "t2.micro"
-	instance   = element(aws_instance.c0m1.*.id)
+	instance   = $(aws_instance.c0m1.id)
+	associate_with_private_ip = "3.220.194.182"
 	key_name = "Project"
 	user_data = "${data.template_file.userdata_client.rendered}"
 	security_groups = [ "project" ]
@@ -30,7 +31,8 @@ resource "aws_eip" "c0m1" {
 resource "aws_eip" "idx-1" {
 	ami = "ami-0affd4508a5d2481b"
 	instance_type = "t2.micro"
-	instance   = element(aws_instance.idx-1.*.id)
+	instance   = $(aws_instance.idx-1.id)
+	associate_with_private_ip = "52.4.66.78"
 	key_name = "Project"
 	user_data = "${data.template_file.userdata_client.rendered}"
 	security_groups = [ "project" ]
@@ -42,7 +44,8 @@ resource "aws_eip" "idx-1" {
 resource "aws_eip" "idx-2" {
 	ami = "ami-0affd4508a5d2481b"
 	instance_type = "t2.micro"
-	instance   = element(aws_instance.idx-2.*.id)
+	instance   = $(aws_instance.idx-2.id)
+	associate_with_private_ip = "54.89.77.140"
 	key_name = "Project"
 	user_data = "${data.template_file.userdata_client.rendered}"
 	security_groups = [ "project" ]
